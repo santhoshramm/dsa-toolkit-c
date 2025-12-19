6e80001dd21cbd769332d711c5ccd86037c65cf4
Santhosh Ram M <santhoshram2006@gmail.com>
2025-12-19 15:54:08 +0530

    Added array reversal implementation

--- arrays/reverse_array.c
+++ arrays/reverse_array.c
@@ -1,1 +1,28 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
+#include <stdio.h>
+
+void reverse(int arr[], int n) {
+    int i = 0, j = n - 1, temp;
+
+    while (i < j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverse(arr, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
