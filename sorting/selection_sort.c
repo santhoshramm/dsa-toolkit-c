067fc2ab54067bc96f88232d685199f7d741c9a3
Santhosh Ram M <santhoshram2006@gmail.com>
2025-12-19 16:03:25 +0530

    Added selection sort algorithm

---
+++ sorting/selection_sort.c
@@ -0,0 +1,25 @@
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+#include <stdio.h>
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min])
+                min = j;
+
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
